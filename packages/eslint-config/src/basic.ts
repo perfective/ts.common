625f078deb44295eb50316ca45614c09d3f19fd6
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-04-22 22:48:52 -0400

    Add stylistics issues to the basic config

--- packages/eslint-config/src/basic.ts
+++ packages/eslint-config/src/basic.ts
@@ -1,8 +1,31 @@
 export = {
+    /* eslint-disable @typescript-eslint/naming-convention */
     extends: './',
     rules: {
+        'function-paren-newline': ['error', 'multiline-arguments'],
+        'max-lines': 'off',
+        'max-lines-per-function': 'off',
+        'max-params': 'off',
+        'max-statements': 'off',
         // Conflicts with decorators
         'new-cap': 'off',
+        'newline-per-chained-call': 'off',
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+        // Traditionally is used to avoid conflicts between property and method name
+        'no-underscore-dangle': 'error',
+        'object-curly-newline': ['error', {
+            ImportDeclaration: { multiline: true },
+            ExportDeclaration: { multiline: true },
+        }],
+        'object-property-newline': ['error', {
+            allowAllPropertiesOnSameLine: true,
+        }],
+        // Compatibility with "@typescript-eslint/triple-slash-reference"
+        'spaced-comment': ['error', 'always', {
+            line: {
+                markers: '/ <reference',
+            }
+        }],
         // Conflicts with decorators
         '@typescript-eslint/no-extraneous-class': 'off',
         // Conflicts with "no-confusing-arrow" { allowParens: true }

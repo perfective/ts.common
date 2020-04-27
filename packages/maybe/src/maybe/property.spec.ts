b151f7d68530765164c1549e795b82c86529fe7a
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-04-26 20:52:38 -0400

    Add hasNotNullProperty()/hasNullProperty()

--- packages/maybe/src/maybe/property.spec.ts
+++ packages/maybe/src/maybe/property.spec.ts
@@ -1,49 +1,97 @@
-import { hasDefinedProperty, hasUndefinedProperty } from './property';
+import {
+    hasDefinedProperty,
+    hasNotNullProperty,
+    hasNullProperty,
+    hasUndefinedProperty,
+} from './property';
 
 interface Example {
-    a?: number;
-    b?: string;
-    c?: boolean;
-    d?: object;
+    a?: number | null;
+    b?: string | null;
+    c?: boolean | null;
+    d?: object | null;
 }
 
-const example: Example = {
+const optional: Example = {
     a: 0,
     b: '',
 };
 
 describe('hasDefinedProperty', () => {
     it('returns true when object property is defined', () => {
-        expect(hasDefinedProperty(example, 'a')).toBe(true);
+        expect(hasDefinedProperty(optional, 'a')).toBe(true);
     });
 
     it('returns false when object property is undefined', () => {
-        expect(hasDefinedProperty(example, 'c')).toBe(false);
+        expect(hasDefinedProperty(optional, 'c')).toBe(false);
     });
 
     it('returns true when all provided properties are defined', () => {
-        expect(hasDefinedProperty(example, 'a', 'b')).toBe(true);
+        expect(hasDefinedProperty(optional, 'a', 'b')).toBe(true);
     });
 
     it('returns false when one of the properties is undefined', () => {
-        expect(hasDefinedProperty(example, 'a', 'c')).toBe(false);
+        expect(hasDefinedProperty(optional, 'a', 'c')).toBe(false);
     });
 });
 
 describe('hasUndefinedProperty', () => {
     it('returns true when object property is undefined', () => {
-        expect(hasUndefinedProperty(example, 'c')).toBe(true);
+        expect(hasUndefinedProperty(optional, 'c')).toBe(true);
     });
 
     it('returns false when object property is undefined', () => {
-        expect(hasUndefinedProperty(example, 'a')).toBe(false);
+        expect(hasUndefinedProperty(optional, 'a')).toBe(false);
     });
 
     it('returns true when all provided properties are undefined', () => {
-        expect(hasUndefinedProperty(example, 'c', 'd')).toBe(true);
+        expect(hasUndefinedProperty(optional, 'c', 'd')).toBe(true);
     });
 
     it('returns false when one of the properties is defined', () => {
-        expect(hasUndefinedProperty(example, 'a', 'c')).toBe(false);
+        expect(hasUndefinedProperty(optional, 'a', 'c')).toBe(false);
+    });
+});
+
+const nullable: Example = {
+    a: 0,
+    b: '',
+    c: null,
+    d: null,
+};
+
+describe('hasNotNullProperty', () => {
+    it('returns true when object property is defined', () => {
+        expect(hasNotNullProperty(nullable, 'a')).toBe(true);
+    });
+
+    it('returns false when object property is undefined', () => {
+        expect(hasNotNullProperty(nullable, 'c')).toBe(false);
+    });
+
+    it('returns true when all provided properties are defined', () => {
+        expect(hasNotNullProperty(nullable, 'a', 'b')).toBe(true);
+    });
+
+    it('returns false when one of the properties is undefined', () => {
+        expect(hasNotNullProperty(nullable, 'a', 'c')).toBe(false);
+    });
+});
+
+describe('hasNullProperty', () => {
+    it('returns true when object property is undefined', () => {
+        expect(hasNullProperty(nullable, 'c')).toBe(true);
+    });
+
+    it('returns false when object property is undefined', () => {
+        expect(hasNullProperty(nullable, 'a')).toBe(false);
+    });
+
+    it('returns true when all provided properties are undefined', () => {
+        expect(hasNullProperty(nullable, 'c', 'd')).toBe(true);
+    });
+
+    it('returns false when one of the properties is defined', () => {
+        expect(hasNullProperty(nullable, 'a', 'c')).toBe(false);
     });
 });
--- packages/maybe/src/maybe/property.ts
+++ packages/maybe/src/maybe/property.ts
@@ -1,7 +1,15 @@
-import { isDefined, isUndefined } from './value';
+import { isDefined, isNotNull, isNull, isUndefined } from './value';
 
 export type Undefined<T> = {
     [P in keyof T]: undefined;
+};
+
+export type NotNull<T> = {
+    [P in keyof T]: T[P] extends null ? never : T[P];
+};
+
+export type Null<T> = {
+    [P in keyof T]: null;
 };
 
 export function hasDefinedProperty<T, K extends keyof T>(
@@ -22,6 +30,28 @@ export function hasUndefinedProperty<T, K extends keyof T>(
     return properties
         .map(property => value[property])
         .map(isUndefined)
+        .filter(not)
+        .length === 0;
+}
+
+export function hasNotNullProperty<T, K extends keyof T>(
+    value: T,
+    ...properties: readonly K[]
+): value is T & NotNull<Pick<T, K>> {
+    return properties
+        .map(property => value[property])
+        .map(isNotNull)
+        .filter(not)
+        .length === 0;
+}
+
+export function hasNullProperty<T, K extends keyof T>(
+    value: T,
+    ...properties: readonly K[]
+): value is T & Null<Pick<T, K>> {
+    return properties
+        .map(property => value[property])
+        .map(isNull)
         .filter(not)
         .length === 0;
 }

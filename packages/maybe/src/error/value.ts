164558dc615c0c19fcb11f744bbd77f72cbc8a9c
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-05-01 22:23:32 -0400

    Fix: explicitly use `undefined` in type guards
    Value argument is required and is `T | undefined`, but not an optional `T`.

--- packages/maybe/src/error/value.ts
+++ packages/maybe/src/error/value.ts
@@ -13,50 +13,38 @@ import { throws } from './error';
 
 export function definedValueOrPanic<T, E extends Error>(
     error?: E | string,
-): (value?: T) => T {
-    return (value?: T): T => valueOrPanic<T | undefined, T, E>(isDefined, value, error);
+): (value: T | undefined) => T {
+    return (value?: T): T => valueOrPanic(isDefined, value, error);
 }
 
 export function undefinedValueOrPanic<T, E extends Error>(
     error?: E | string,
-): (value?: T) => undefined {
-    return (value?: T): undefined => valueOrPanic<T | undefined, undefined, E>(
-        isUndefined,
-        value,
-        error,
-    );
+): (value: T | undefined) => undefined {
+    return (value?: T): undefined => valueOrPanic(isUndefined, value, error);
 }
 
 export function notNullValueOrPanic<T, E extends Error>(
     error?: E | string,
 ): (value: T | null) => T {
-    return (value: T | null): T => valueOrPanic<T | null, T, E>(isNotNull, value, error);
+    return (value: T | null): T => valueOrPanic(isNotNull, value, error);
 }
 
 export function nullValueOrPanic<T, E extends Error>(
     error?: E | string,
 ): (value: T | null) => null {
-    return (value: T | null): null => valueOrPanic<T | null, null, E>(isNull, value, error);
+    return (value: T | null): null => valueOrPanic(isNull, value, error);
 }
 
 export function presentValueOrPanic<T, E extends Error>(
     error?: E | string,
 ): (value?: T | null) => T {
-    return (value?: T | null): T => valueOrPanic<T | Absent, T, E>(
-        isPresent,
-        value,
-        error,
-    );
+    return (value: T | Absent): T => valueOrPanic(isPresent, value, error);
 }
 
 export function absentValueOrPanic<T, E extends Error>(
     error?: E | string,
 ): (value?: T | null) => Absent {
-    return (value?: T | null): Absent => valueOrPanic<T | Absent, Absent, E>(
-        isAbsent,
-        value,
-        error,
-    );
+    return (value: T | Absent): Absent => valueOrPanic(isAbsent, value, error);
 }
 
 function valueOrPanic<T, V extends T, E extends Error>(
--- packages/maybe/src/value/value.ts
+++ packages/maybe/src/value/value.ts
@@ -1,15 +1,16 @@
 export type Absent = undefined | null;
+export type Present<T> = T extends Absent ? never : T;
 
 export type Unary<T, U> = (value: T) => U;
 export type Predicate<T> = (value: T) => boolean;
 export type TypeGuard<T, V extends T> = (value: T) => value is V;
 
-export function isDefined<T>(value?: T): value is T {
+export function isDefined<T>(value: T | undefined): value is T {
     // eslint-disable-next-line no-void
     return value !== void 0;
 }
 
-export function isUndefined<T>(value?: T): value is undefined {
+export function isUndefined<T>(value: T | undefined): value is undefined {
     // eslint-disable-next-line no-void
     return value === void 0;
 }
@@ -22,10 +23,10 @@ export function isNull<T>(value: T | null): value is null {
     return value === null;
 }
 
-export function isPresent<T>(value?: T | null): value is T {
+export function isPresent<T>(value: T | undefined | null): value is T {
     return isDefined(value) && isNotNull(value);
 }
 
-export function isAbsent<T>(value?: T | null): value is Absent {
+export function isAbsent<T>(value: T | undefined | null): value is undefined | null {
     return isUndefined(value) || isNull(value);
 }

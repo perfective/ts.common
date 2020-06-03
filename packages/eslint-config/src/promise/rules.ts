1faab927602b61852230ad68bdf29697984e8990
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-06-03 00:53:49 -0400

    Use eslint-plugin-promise

---
+++ packages/eslint-config/src/promise/rules.ts
@@ -0,0 +1,16 @@
+export = {
+    rules: {
+        'promise/always-return': 'error',
+        'promise/avoid-new': 'off',
+        'promise/catch-or-return': 'error',
+        'promise/no-callback-in-promise': 'error',
+        'promise/no-native': 'off',
+        'promise/no-nesting': 'error',
+        'promise/no-new-statics': 'error',
+        'promise/no-promise-in-callback': 'error',
+        'promise/no-return-in-finally': 'error',
+        'promise/no-return-wrap': 'error',
+        'promise/param-names': 'error',
+        'promise/valid-params': 'error',
+    },
+};

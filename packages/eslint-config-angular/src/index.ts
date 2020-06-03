75c406aee8e6f828e4dabeebc381a3ae19585614
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-06-03 03:08:58 -0400

    Disable node/file-extension-in-import for Angular

--- packages/eslint-config-angular/src/index.ts
+++ packages/eslint-config-angular/src/index.ts
@@ -2,4 +2,8 @@ export = {
     extends: [
         '@perfective/eslint-config',
     ],
+    rules: {
+        // Rule conflicts with the Angular file suffixes: .module, .component, etc.
+        'node/file-extension-in-import': 'off',
+    }
 };

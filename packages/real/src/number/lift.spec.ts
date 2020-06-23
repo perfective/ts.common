3edee00bbd2e411f0fddb1244da8c2fc44c38783
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-06-23 00:58:25 -0400

    Add Digits and exponential() function

--- packages/real/src/number/lift.spec.ts
+++ packages/real/src/number/lift.spec.ts
@@ -1,4 +1,32 @@
-import { precision } from './lift';
+import { exponential, precision } from './lift';
+
+describe('exponential', () => {
+    describe('exponential(n)', () => {
+        it('returns a string representation of a number in exponential notation', () => {
+            expect(exponential(0)(3.141_592_653_589_793))
+                .toStrictEqual('3e+0');
+            expect(exponential(0)(3_141_592_653_589_793))
+                .toStrictEqual('3e+15');
+            expect(exponential(15)(3.141_592_653_589_793))
+                .toStrictEqual('3.141592653589793e+0');
+            expect(exponential(16)(3_141_592_653_589_793_2))
+                .toStrictEqual('3.1415926535897932e+16');
+        });
+
+        /* eslint-disable @typescript-eslint/no-loss-of-precision -- precision failure test */
+        it('only keeps about 17 decimal places of precision', () => {
+            expect(exponential(16)(3.141_592_653_589_793_2))
+                .toStrictEqual('3.1415926535897931e+0');
+            expect(exponential(17)(3_141_592_653_589_793_23))
+                .toStrictEqual('3.14159265358979328e+17');
+            expect(exponential(20)(3.141_592_653_589_793_238_46))
+                .toStrictEqual('3.14159265358979311600e+0');
+            expect(exponential(20)(3_141_592_653_589_793_238_46))
+                .toStrictEqual('3.14159265358979334144e+20');
+        });
+        /* eslint-enable @typescript-eslint/no-loss-of-precision */
+    });
+});
 
 describe('precision', () => {
     describe('precision(n)', () => {
--- packages/real/src/number/lift.ts
+++ packages/real/src/number/lift.ts
@@ -1,7 +1,14 @@
 import { Unary } from '@perfective/fp';
 
+export type Digits = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+| 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+
 export type Precision = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21;
+
+export function exponential(fraction: Digits): Unary<number, string> {
+    return (value: number): string => value.toExponential(fraction);
+}
 
 export function precision(precision: Precision): Unary<number, string> {
     return (value: number): string => value.toPrecision(precision);

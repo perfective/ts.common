ee6469d94cce322398906bc049ab0200d536b2cd
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-11-26 14:44:23 -0500

    Upgrade: eslint-plugin-jest-dom@3.3.0

--- packages/eslint-config-angular/src/jest-dom/rules.ts
+++ packages/eslint-config-angular/src/jest-dom/rules.ts
@@ -4,6 +4,9 @@ export = {
         'jest-dom/prefer-empty': 'warn',
         'jest-dom/prefer-enabled-disabled': 'warn',
         'jest-dom/prefer-focus': 'warn',
+        // TODO: Set to "warn" once the rule is fixed
+        //  https://github.com/testing-library/eslint-plugin-jest-dom/pull/105
+        'jest-dom/prefer-in-document': 'off',
         'jest-dom/prefer-required': 'warn',
         'jest-dom/prefer-to-have-attribute': 'warn',
         'jest-dom/prefer-to-have-style': 'warn',

3b11cc8a6009992c9bffee7222dafa1af6633d2c
Andrey Mikheychik <a.mikheychik@gmail.com>
2020-12-15 11:51:48 -0500

    Mark Codelyzer rules that have ESLint Angular replacement as deprecated

--- packages/eslint-config-angular/codelyzer/index.ts
+++ packages/eslint-config-angular/codelyzer/index.ts
@@ -5,15 +5,23 @@ const functionality: Rules = {
     'template-accessibility-alt-text': true,
     'template-accessibility-elements-content': true,
     'template-accessibility-label-for': true,
+
+    /** @deprecated - see @angular-eslint/template/accessibility-tabindex-no-positive. */
     'template-accessibility-tabindex-no-positive': true,
     'template-accessibility-table-scope': true,
     'template-accessibility-valid-aria': true,
+
+    /** @deprecated - see @angular-eslint/template/banana-in-a-box. */
     'template-banana-in-box': true,
     'template-click-events-have-key-events': true,
     'template-mouse-events-have-key-events': true,
     'template-no-any': true,
+
+    /** @deprecated - see @angular-eslint/template/no-autofocus. */
     'template-no-autofocus': true,
     'template-no-distracting-elements': true,
+
+    /** @deprecated - see @angular-eslint/template/no-negated-async. */
     'template-no-negated-async': true,
 };
 
@@ -21,7 +29,11 @@ const maintainability: Rules = {
     'no-unused-css': true,
     'template-conditional-complexity': [true, 2],
     'template-i18n': false,
+
+    /** @deprecated - see @angular-eslint/template/no-call-expression. */
     'template-no-call-expression': true,
+
+    /** @deprecated - see @angular-eslint/template/use-track-by-functions. */
     'template-use-track-by-function': true,
 };
 

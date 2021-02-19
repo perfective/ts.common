c38a29a87f62b364bd8d2b01e18592872821b9d4
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-02-18 21:50:28 -0500

    Add `isFlagOn`/`hasFlagOn` bitmask predicates

--- packages/real/src/bitmask/bitmask.spec.ts
+++ packages/real/src/bitmask/bitmask.spec.ts
@@ -1,6 +1,16 @@
 import { Enum } from '../enum/enum';
 
-import { Bitmask } from './bitmask';
+import { Bitmask, hasFlagOn, isFlagOn } from './bitmask';
+
+enum BitBool {
+    True = 0b1111_1111,
+    False = 0,
+}
+
+enum CharBool {
+    True = 't',
+    False = 'f',
+}
 
 describe('bitmask<T>', () => {
     it('can be created without a generic type parameter', () => {
@@ -10,25 +20,53 @@ describe('bitmask<T>', () => {
     });
 
     it('must be used with a number-based enum', () => {
-        enum Bool {
-            True = -1,
-            False = 0,
-        }
+        const truth: Bitmask<Enum<BitBool>> = BitBool.True;
 
-        const truth: Bitmask<Enum<Bool>> = Bool.True;
-
-        expect(truth).toBe(-1);
+        expect(truth).toBe(255);
     });
 
     it('cannot be used with a string-based enum', () => {
-        enum Bool {
-            True = 'true',
-            False = 'false',
-        }
-
         // @ts-expect-error -- Type 'Record<string, Bool>' does not satisfy the constraint 'Record<string, number>'
-        const truth: Bitmask<Enum<Bool>> = Bool.True;
+        const truth: Bitmask<Enum<CharBool>> = CharBool.True;
 
-        expect(truth).toBe('true');
+        expect(truth).toBe('t');
+    });
+});
+
+describe('isFlagOn', () => {
+    it('returns true when all of the given flags are raised on the bitmask', () => {
+        expect(isFlagOn(0b0000_0010, 0b0000_0010))
+            .toBe(true);
+        expect(isFlagOn(0b0110_0011, 0b0110_0010))
+            .toBe(true);
+    });
+
+    it('returns false when any the given flag is not raised on the bitmask', () => {
+        expect(isFlagOn(0b1001_0110, 0b0100_1001))
+            .toBe(false);
+    });
+
+    it('returns false when flag is 0', () => {
+        expect(isFlagOn(BitBool.True, BitBool.False))
+            .toBe(false);
+    });
+});
+
+describe('hasFlagOn', () => {
+    it('returns true when all of the given flags are raised on the bitmask', () => {
+        expect(hasFlagOn(0b0000_0010)(0b0000_0010))
+            .toBe(true);
+        expect(hasFlagOn(0b0110_0010 as number)(0b0110_0011))
+            .toBe(true);
+    });
+
+    it('returns false when any the given flag is not raised on the bitmask', () => {
+        expect(hasFlagOn(0b0000_0010 as number)(0b0000_0001))
+            .toBe(false);
+    });
+
+    it('returns false when flag is 0', () => {
+        expect(hasFlagOn<BitBool>(BitBool.False)(BitBool.True))
+            .toBe(false);
     });
 });
--- packages/real/src/bitmask/bitmask.ts
+++ packages/real/src/bitmask/bitmask.ts
@@ -1,3 +1,28 @@
+import { Unary } from '@perfective/fp';
+
 import { Enum } from '../enum/enum';
 
 export type Bitmask<T extends Enum<number> | number = number> = T extends Enum<number> ? T[keyof T] : number;
+
+/**
+ * Creates a curried version of the hasRaised() function.
+ *
+ * @see isFlagOn()
+ */
+export function hasFlagOn<T extends Bitmask = number>(flag: T): Unary<T, boolean> {
+    return (bitmask: T): boolean => isFlagOn(bitmask, flag);
+}
+
+/* eslint-disable no-bitwise -- bitmask operations require bitwise operators */
+
+/**
+ * Returns true when given flags are raised on a bitmask.
+ */
+export function isFlagOn<T extends Bitmask = Bitmask>(bitmask: T, flag: T): boolean {
+    /* eslint-disable @typescript-eslint/tslint/config -- flag is a number */
+    return (bitmask & flag) === flag
+        && flag !== 0;
+    /* eslint-enable @typescript-eslint/tslint/config */
+}
+
+/* eslint-enable no-bitwise */

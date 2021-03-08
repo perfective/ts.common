790e560c1aa0b1accadf877c8d4695f16be14ded
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-03-08 05:15:34 -0500

    Add a placeholder code into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-// eslint-disable-next-line import/unambiguous -- placeholder file
+/**
+ * A placeholder for https://www.npmjs.com/package/is-esm to recognize the module.
+ */
+export const name = '@perfective/common';

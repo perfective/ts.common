e4c6e81b3cb0e2c31b71f0052a17d35b9e4e67b9
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-06-06 02:21:24 -0400

    Setup prettier v2.3.1

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'strict',
+    jsxBracketSameLine: true,
+    jsxSingleQuote: false,
+    printWidth: 120,
+    proseWrap: 'preserve',
+    quoteProps: 'consistent',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    vueIndentScriptAndStyle: true
+};

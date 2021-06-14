2d9d63b6ce0708be65c974b58a218745e154a271
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-06-14 02:31:46 -0400

    Update @perfective/build to v0.1.0-beta
    Use Prettier and Jest configurations from @perfective/build

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,3 @@
-module.exports = {
-    arrowParens: 'avoid',
-    bracketSpacing: true,
-    embeddedLanguageFormatting: 'auto',
-    endOfLine: 'lf',
-    htmlWhitespaceSensitivity: 'strict',
-    jsxBracketSameLine: true,
-    jsxSingleQuote: false,
-    printWidth: 120,
-    proseWrap: 'preserve',
-    quoteProps: 'consistent',
-    semi: true,
-    singleQuote: true,
-    tabWidth: 4,
-    trailingComma: 'all',
-    vueIndentScriptAndStyle: true
-};
+const perfective = require('@perfective/build/prettier');
+
+module.exports = perfective;
--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,3 @@
-module.exports = {
-    testEnvironment: 'node',
-    preset: 'ts-jest',
-    rootDir: './src',
-    globals: {
-        'ts-jest': {
-            tsconfig: './tsconfig.json',
-        },
-    },
-    collectCoverage: true,
-    coverageReporters: ['text'],
-    coverageThreshold: {
-        global: {
-            statements: 100,
-            branches: 100,
-            functions: 100,
-            lines: 100,
-        },
-    },
-    verbose: false,
-};
+const perfective = require('@perfective/build/jest');
+
+module.exports = perfective;

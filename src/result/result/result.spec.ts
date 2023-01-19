2b646c1180375dba11592cd6740dfe0f3a65b212
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-01-19 06:31:07 -0600

    Parameterize `Result` specs and reduce duplication

--- src/result/result/result.spec.ts
+++ src/result/result/result.spec.ts
@@ -1,8 +1,7 @@
 import { array } from '../../array/array/array';
 import { error } from '../../error/error/error';
 import { chainedException } from '../../error/exception/exception';
-import { same } from '../../function/function/unary';
-import { output as stringOutput } from '../../string/string/output';
+import { same, Unary } from '../../function/function/unary';
 
 import { Failure, failure, Result, result, Success, success } from './result';
 import {
@@ -51,29 +50,41 @@ describe(result, () => {
     });
 });
 
-describe(Result, () => {
+/**
+ * The {@linkcode Result} is an abstract class, so the tests for it use {@linkcode Success} and {@linkcode Failure}.
+ * The purpose of each test is only to show any relations between methods.
+ * Each test should use at least three inputs:
+ * - a {@linkcode Success} with a non-error value,
+ * - a {@linkcode Success} with an {@linkcode Error} value,
+ * - and a {@linkcode Failure}.
+ *
+ * Some parameters are duplicated to preserve generic form.
+ */
+describe.each([
+    // Success with a non-error value
+    [success(0), resultDecimal, resultNumber, strictNumberOutput, array, safeNumberOutput],
+    // Success with an error value
+    [success(error('Success')), successErrorMessage, resultNumber, strictErrorOutput, array, safeNumberOutput],
+    // Failure (with an error value)
+    [failure<number>(error('Failure')), resultDecimal, resultNumber, strictNumberOutput, array, safeNumberOutput],
+    // @ts-expect-error -- TSC creates a union of all types, while only each row arguments have to match.
+])(Result.name, <T, U1, V1, U2, V2, U3>(
+    result: Result<T>,
+    flatMap1: Unary<T, Result<U1>>,
+    flatMap2: Unary<U1, Result<V1>>,
+    mapValue1: Unary<T, U2>,
+    mapValue2: Unary<U2, V2>,
+    reduce: Unary<T | Error, U3>,
+) => {
+    const mapError1 = chainedException('Exceptional');
+    const mapError2 = chainedException('Failure');
+
     describe('onto', () => {
         describe('is a "bind" (>>=) operator for the Result monad', () => {
             it('is associative', () => {
-                // Associativity: ma >>= (\x -> f x >>= g)  ===  (ma >>= k) >>= g
-
-                // Success with a non-error value
-                const sa = success(0);
-
-                expect(sa.onto(x => resultDecimal(x).onto(resultNumber)))
-                    .toStrictEqual(sa.onto(resultDecimal).onto(resultNumber));
-
-                // Success with an error value
-                const sb = success(error('Success'));
-
-                expect(sb.onto(x => successErrorMessage(x).onto(resultNumber)))
-                    .toStrictEqual(sb.onto(successErrorMessage).onto(resultNumber));
-
-                // Failure (with an error value)
-                const fa = failure<number>(error('Failure'));
-
-                expect(fa.onto(x => resultDecimal(x).onto(resultNumber)))
-                    .toStrictEqual(fa.onto(resultDecimal).onto(resultNumber));
+                // Associativity: ma >>= (\x -> f x >>= g)  ===  (ma >>= f) >>= g
+                expect(result.onto(x => flatMap1(x).onto(flatMap2)))
+                    .toStrictEqual(result.onto(flatMap1).onto(flatMap2));
             });
         });
     });
@@ -84,45 +95,16 @@ describe(Result, () => {
                 // Identity:
                 // - fmap id  ===  id
                 // - second id  ===  id
-
-                // Success with a non-error value
-                const sa = success(0);
-
-                expect(sa.to(same)).toStrictEqual(same(sa));
-
-                // Success with an error value
-                const sb = success(error('Success'));
-
-                expect(sb.to(same)).toStrictEqual(same(sb));
-
-                // Failure (with an error value)
-                const fa = failure<number>(error('Failure'));
-
-                expect(fa.to(same)).toStrictEqual(same(fa));
+                expect(result.to(same))
+                    .toStrictEqual(same(result));
             });
 
             it('preserves composition of morphisms', () => {
                 // Composition:
                 // - fmap (f . g)  ===  fmap f . fmap g
                 // - second (f . g)  ===  second f . second g
-
-                // Success with a non-error value
-                const sa = success(0);
-
-                expect(sa.to(x => array(stringOutput(x))))
-                    .toStrictEqual(sa.to(stringOutput).to(array));
-
-                // Success with an error value
-                const sb = success(error('Success'));
-
-                expect(sb.to(x => array(successErrorMessage(x))))
-                    .toStrictEqual(sb.to(successErrorMessage).to(array));
-
-                // Failure (with an error value)
-                const fa = failure<number>(error('Failure'));
-
-                expect(fa.to(x => array(stringOutput(x))))
-                    .toStrictEqual(fa.to(stringOutput).to(array));
+                expect(result.to(x => mapValue2(mapValue1(x))))
+                    .toStrictEqual(result.to(mapValue1).to(mapValue2));
             });
         });
     });
@@ -131,80 +113,27 @@ describe(Result, () => {
         describe('is a "bimap" method for the Result bifunctor', () => {
             it('preserves identity morphisms', () => {
                 // Identity: bimap id id = id
-
-                // Success with a non-error value
-                const sa = success(0);
-
-                expect(sa.to(same, same)).toStrictEqual(same(sa));
-
-                // Success with an error value
-                const sb = success(error('Success'));
-
-                expect(sb.to(same, same)).toStrictEqual(same(sb));
-
-                // Failure (with an error value)
-                const fa = failure<number>(error('Failure'));
-
-                expect(fa.to(same, same)).toStrictEqual(same(fa));
+                expect(result.to(same, same))
+                    .toStrictEqual(same(result));
             });
 
             it('the same as applying the "first" and "second" methods', () => {
                 // Ensure: bimap f g  =  first f . second g
-
-                // Success with a non-error value
-                const sa = success(0);
-
-                expect(sa.to(strictNumberOutput, chainedException('Exceptional')))
-                    .toStrictEqual(sa.to(strictNumberOutput).failure(chainedException('Exceptional')));
-
-                // Success with an error value
-                const sb = success(error('Success'));
-
-                expect(sb.to(strictErrorOutput, chainedException('Exceptional')))
-                    .toStrictEqual(sb.to(strictErrorOutput).failure(chainedException('Exceptional')));
-
-                // Failure (with an error value)
-                const fa = failure<number>(error('Failure'));
-
-                expect(fa.to(strictNumberOutput, chainedException('Exceptional')))
-                    .toStrictEqual(fa.to(strictNumberOutput).failure(chainedException('Exceptional')));
+                expect(result.to(mapValue1, mapError1))
+                    .toStrictEqual(result.to(mapValue1).failure(mapError1));
             });
         });
     });
 
     describe('into(reduce)', () => {
         it('is an equivalent of applying a `reduce` callback to the Result.value', () => {
-            // Success with a non-error value
-            const sa = success(0);
-
-            expect(sa.into(safeNumberOutput)).toBe(safeNumberOutput(sa.value));
-
-            // Success with an error value
-            const sb = success(error('Success'));
-
-            expect(sb.into(safeNumberOutput)).toBe(safeNumberOutput(sb.value));
-
-            // Failure (with an error value)
-            const fa = failure<number>(error('Failure'));
-
-            expect(fa.into(safeNumberOutput)).toBe(safeNumberOutput(fa.value));
+            expect(result.into(reduce))
+                .toBe(reduce(result.value));
         });
 
         it('is an equivalent of applying a `reduce` callback as `reduceValue` and `reduceError`', () => {
-            // Success with a non-error value
-            const sa = success(0);
-
-            expect(sa.into(safeNumberOutput)).toBe(sa.into(safeNumberOutput, safeNumberOutput));
-
-            // Success with an error value
-            const sb = success(error('Success'));
-
-            expect(sb.into(safeNumberOutput)).toBe(sb.into(safeNumberOutput, safeNumberOutput));
-
-            // Failure (with an error value)
-            const fa = failure<number>(error('Failure'));
-
-            expect(fa.into(safeNumberOutput)).toBe(fa.into(safeNumberOutput, safeNumberOutput));
+            expect(result.into(reduce))
+                .toBe(result.into(reduce, reduce));
         });
     });
 
@@ -212,45 +141,14 @@ describe(Result, () => {
         describe('is a "first" method of a Result bifunctor', () => {
             it('preserves identity morphisms', () => {
                 // Identity: first id  =  id
-
-                // Success with a non-error value
-                const sa = success(0);
-
-                expect(sa.failure(same)).toStrictEqual(same(sa));
-
-                // Success with an error value
-                const sb = success(error('Success'));
-
-                expect(sb.failure(same)).toStrictEqual(same(sb));
-
-                // Failure (with an error value)
-                const fa = failure<number>(error('Failure'));
-
-                expect(fa.failure(same)).toStrictEqual(same(fa));
+                expect(result.failure(same))
+                    .toStrictEqual(same(result));
             });
 
             it('preserves composition of morphisms', () => {
                 // Composition: first (f . g)  ===  first f . first g
-                const f = chainedException('Exceptional');
-                const g = chainedException('Failure');
-
-                // Success with a non-error value
-                const sa = success(0);
-
-                expect(sa.failure(error => f(g(error))))
-                    .toStrictEqual(sa.failure(g).failure(f));
-
-                // Success with an error value
-                const sb = success(error('Success'));
-
-                expect(sb.failure(error => f(g(error))))
-                    .toStrictEqual(sb.failure(g).failure(f));
-
-                // Failure (with an error value)
-                const fa = failure<number>(error('Failure'));
-
-                expect(fa.failure(error => f(g(error))))
-                    .toStrictEqual(fa.failure(g).failure(f));
+                expect(result.failure(error => mapError2(mapError1(error))))
+                    .toStrictEqual(result.failure(mapError1).failure(mapError2));
             });
         });
     });

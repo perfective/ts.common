7d6e69cb20f415688d048c625f97a073f5635ae7
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-02-28 06:53:59 -0600

    Remove eslint config overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,4 @@ module.exports = {
     extends: [
         '@perfective/eslint-config',
     ],
-    overrides: [
-        {
-            files: ['*.[jt]s?(x)'],
-            rules: {
-                'jsdoc/require-description': ['error', {
-                    exemptedBy: ['inheritdoc', 'package', 'private', 'see', 'deprecated'],
-                }],
-            },
-        },
-    ],
 };

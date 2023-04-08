0f4b71023f3cce16459b94b159f7f70a61177f0c
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-04-08 18:36:36 -0500

    Add Enum documentation

--- src/object/enum/enum.spec.ts
+++ src/object/enum/enum.spec.ts
@@ -23,23 +23,20 @@ enum MixedDirection {
 }
 /* eslint-enable @typescript-eslint/no-mixed-enums */
 
-describe('members', () => {
+describe(members, () => {
     it('returns keys of a numeric enum', () => {
-        // "keyof Direction" are keys of a Number.
         const directions: Member<NumericDirection>[] = members(NumericDirection);
 
         expect(directions).toStrictEqual(['Up', 'Down', 'Left', 'Right']);
     });
 
     it('returns keys of a string enum', () => {
-        // "keyof Direction" are keys of a Number.
         const directions: Member<StringDirection>[] = members(StringDirection);
 
         expect(directions).toStrictEqual(['Up', 'Down', 'Left', 'Right']);
     });
 
     it('returns keys of a mixed enum', () => {
-        // "keyof Direction" are keys of a Number.
         const directions: Member<MixedDirection>[] = members(MixedDirection);
 
         expect(directions).toStrictEqual(['Up', 'Right', 'Down', 'Left']);

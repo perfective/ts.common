8fb5b5e1b8b0d9c0c5b053b47c07b65ccac70aac
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-09-06 23:40:27 -0500

    Support string input parsing in `timestamp()`

--- src/date/date/timestamp.spec.ts
+++ src/date/date/timestamp.spec.ts
@@ -2,15 +2,35 @@ import { epoch } from './date';
 import { timestamp } from './timestamp';
 
 describe(timestamp, () => {
-    describe('when given a valid Date', () => {
-        it('returns a Timestamp for the given Date', () => {
-            expect(timestamp(epoch())).toBe(0);
+    describe('timestamp(Date)', () => {
+        describe('when given a valid Date', () => {
+            it('returns a Timestamp for the given Date', () => {
+                expect(timestamp(epoch())).toBe(0);
+            });
+        });
+
+        describe('when given an invalid Date', () => {
+            it('returns null', () => {
+                expect(timestamp(new Date('xyz'))).toBeNull();
+            });
         });
     });
 
-    describe('when given an invalid Date', () => {
-        it('returns null', () => {
-            expect(timestamp(new Date('xyz'))).toBeNull();
+    describe('timestamp(string)', () => {
+        describe('when given a parseable date/time string', () => {
+            it('returns a Timestamp for the given date/time', () => {
+                expect(timestamp('1970-01-01T00:00:00.000Z')).toBe(0);
+                // January 1, 1970, 00:10 UTC is 10 minutes past the epoch.
+                expect(timestamp('1970-01-01T00:10+00:00')).toBe(10 * 60 * 1000);
+                // Midnight, January 2, 1970, UTC is 24 hours past the epoch.
+                expect(timestamp('1970-01-02')).toBe(24 * 60 * 60 * 1000);
+            });
+        });
+
+        describe('when given a non-parseable date/time string', () => {
+            it('returns null', () => {
+                expect(timestamp('1970-01-02+00:00')).toBeNull();
+            });
         });
     });
 });
--- src/date/date/timestamp.ts
+++ src/date/date/timestamp.ts
@@ -1,3 +1,5 @@
+import { isString } from '../../string/string/string';
+
 /**
  * An integer value representing the number of milliseconds since January 1, 1970 00:00 UTC.
  */
@@ -10,8 +12,25 @@ export type Timestamp = number;
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTime
  */
-export function timestamp(date: Date): Timestamp | null {
-    const output = date.getTime();
+export function timestamp(date: Date): Timestamp | null;
+
+/**
+ * Parses a given date/time `input` string as a {@link Timestamp}.
+ *
+ * If the given `input` is invalid, returns `null`.
+ *
+ * Use instead of the `Date.parse()` function,
+ * as it's easy to compose using the `maybe()` monad.
+ *
+ * @param input - A string in the date/time string format.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#date_time_string_format
+ */
+export function timestamp(input: string): Timestamp | null;
+
+export function timestamp(value: Date | string): Timestamp | null {
+    const output = isString(value) ? Date.parse(value) : value.getTime();
     if (Number.isNaN(output)) {
         return null;
     }

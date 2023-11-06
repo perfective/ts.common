39dada38107f6821da24c498a225a7f70784cef8
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-11-06 08:30:02 -0600

    Use correct separator for MacOS strings
    MacOS line separator is `\r`, not `\n\r`.

--- src/string/string/array.spec.ts
+++ src/string/string/array.spec.ts
@@ -1,13 +1,53 @@
 import { lines } from './array';
 
-describe('lines', () => {
-    it('splits string into array of string by line break characters', () => {
-        expect(lines('Lorem\r\nipsum\ndolor\tsit\n\ramet'))
-            .toStrictEqual([
+describe(lines, () => {
+    describe('when a given string contains Windows line separators (\\r\\n)', () => {
+        it('splits the string into an array of strings', () => {
+            expect(lines('Lorem\r\nipsum\r\ndolor\r\nsit\r\namet')).toStrictEqual([
                 'Lorem',
                 'ipsum',
-                'dolor\tsit',
+                'dolor',
+                'sit',
                 'amet',
             ]);
+        });
+    });
+
+    describe('when a given string contains Unix line separators (\\n)', () => {
+        it('splits the string into an array of strings', () => {
+            expect(lines('Lorem\nipsum\ndolor\nsit\namet')).toStrictEqual([
+                'Lorem',
+                'ipsum',
+                'dolor',
+                'sit',
+                'amet',
+            ]);
+        });
+    });
+
+    describe('when a given string contains MacOS separators (\\r)', () => {
+        it('splits the string into an array of strings', () => {
+            expect(lines('Lorem\ripsum\rdolor\rsit\ramet')).toStrictEqual([
+                'Lorem',
+                'ipsum',
+                'dolor',
+                'sit',
+                'amet',
+            ]);
+        });
+    });
+
+    describe('when string contains mixed separators', () => {
+        it('splits string into array of string by line break characters', () => {
+            expect(lines('Lorem\r\nipsum\ndolor\n\rsit\ramet'))
+                .toStrictEqual([
+                    'Lorem',
+                    'ipsum',
+                    'dolor',
+                    '',
+                    'sit',
+                    'amet',
+                ]);
+        });
     });
 });
--- src/string/string/array.ts
+++ src/string/string/array.ts
@@ -1,3 +1,8 @@
+/**
+ * Splits a given string into an array of string based on the line separator (`\r\n\`, `\n`, and `\r`).
+ *
+ * @since v0.2.0
+ */
 export function lines(value: string): string[] {
-    return value.split(/\r\n|\n\r|\n/gu);
+    return value.split(/\r\n|\r|\n/gu);
 }
